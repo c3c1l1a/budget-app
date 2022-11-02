9853ac635ff290df24261c3a429ecc3bb548b4e8
c3c1l1a <cesswangumukima@gmail.com>
2022-11-02 12:57:09 +0800

    Add pending assertion for the expenses model

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Expense, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it 'name should be present'
+  it 'name should not be blank'
+  it 'name should be between 3 - 10 characters'
+  it 'icon should be present'
+  it 'icon should not be blank'
+  it 'icon should longer than 3 characters'
+  it 'description should be present'
+  it 'description should not be blank'
+  it 'description should longer than 3 characters'
+  it 'amount should not be less than 0'
+  it 'amount should not be nil' 
 end
+
