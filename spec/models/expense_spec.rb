968baf70dd44c5a090fb1d9b7d75e2024e12eed8
c3c1l1a <cesswangumukima@gmail.com>
2022-11-02 13:22:48 +0800

    Test Expense icon should be present

--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -24,8 +24,13 @@ RSpec.describe Expense, type: :model do
     subject.name = 'na'
     expect(subject).to_not be_valid
   end
+
+  it 'icon should be present' do
+    subject.icon = nil
+    expect(subject).to_not be_valid 
+  end 
+
   
-  it 'icon should be present'
   it 'icon should not be blank'
   it 'icon should longer than 3 characters'
   it 'description should be present'
