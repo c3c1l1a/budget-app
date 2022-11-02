e68a1d3b5fd264795c002344c3d39920f048cb54
c3c1l1a <cesswangumukima@gmail.com>
2022-11-02 13:44:36 +0800

    Update expense validation to check for float numbers

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -5,5 +5,5 @@ class Expense < ApplicationRecord
   validates :icon, presence: { message: "Icon can't be null" }, length: { minimum: 3 }
   validates :description, presence: { message: "Description can't be null"}, length: {minimum: 3}
   validates :amount, presence: { message: "Amount can't be null" }
-  validates :amount, numericality: { only_integer: true, greater_than: 0, message: 'Amount must be greater than 0' }
+  validates :amount, numericality: { only_float: true, greater_than: 0, message: 'Amount must be greater than 0' }
 end
