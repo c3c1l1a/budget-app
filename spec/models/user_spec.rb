4d11d5f09d577250153cc70c46a3182b24f74fc9
c3c1l1a <cesswangumukima@gmail.com>
2022-11-02 13:53:19 +0800

    Test User has_many association with expenses

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  # test associtions
+  describe 'associations' do
+    it 'has many expenses' do
+      assc = described_class.reflect_on_association(:expenses)
+      expect(assc.macro).to eq :has_many
+    end
+  end
+
   # test validations.
   describe 'model validations' do
     it 'is not valid without a name' do
