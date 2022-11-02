ff8b53be5bef69510b7650faa2cadb472304cb03
c3c1l1a <cesswangumukima@gmail.com>
2022-11-02 13:57:36 +0800

    Add has_many association to expenses in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_many :expenses, foreign_key: :user_id, dependent: :destroy
+
   validates :name, presence: true
   validates :password, presence: true, length: { in: 6..25 }
   validates :email, presence:true
