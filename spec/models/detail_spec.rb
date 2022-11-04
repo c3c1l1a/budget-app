39959d65d08df31f063654a134d3d134e5c0ab68
c3c1l1a <cesswangumukima@gmail.com>
2022-11-04 12:31:31 +0800

    Test expense has many details

---
+++ spec/models/detail_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe Detail, type: :model do
+  @user = User.new(name: 'Cecilia', email: 'test@gmail.com', password: '123456')
+end
--- spec/models/expense_spec.rb
+++ spec/models/expense_spec.rb
@@ -13,58 +13,69 @@ RSpec.describe Expense, type: :model do
   end
   before { subject.save }
 
-  it 'name should be present' do
-    subject.name = nil
-    expect(subject).to_not be_valid
+  describe 'associations' do
+    it 'has many details' do
+      assc = described_class.reflect_on_association(:details)
+      expect(assc.macro).to eq :has_many
+    end
   end
 
-  it 'name should not be blank' do
-    subject.name = ''
-    expect(subject).to_not be_valid
-  end
+  # test validations.
+  describe 'model validations' do
+    it 'name should be present' do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
 
-  it 'name should be between 3 - 10 characters' do
-    subject.name = 'na'
-    expect(subject).to_not be_valid
-  end
+    it 'name should not be blank' do
+      subject.name = ''
+      expect(subject).to_not be_valid
+    end
 
-  it 'icon should be present' do
-    subject.icon = nil
-    expect(subject).to_not be_valid
-  end
+    it 'name should be between 3 - 10 characters' do
+      subject.name = 'na'
+      expect(subject).to_not be_valid
+    end
 
-  it 'icon should not be blank' do
-    subject.icon = ''
-    expect(subject).to_not be_valid
-  end
+    it 'icon should be present' do
+      subject.icon = nil
+      expect(subject).to_not be_valid
+    end
 
-  it 'icon should longer than 3 characters' do
-    subject.icon = 'ic'
-    expect(subject).to_not be_valid
-  end
+    it 'icon should not be blank' do
+      subject.icon = ''
+      expect(subject).to_not be_valid
+    end
 
-  it 'description should be present' do
-    subject.description = nil
-    expect(subject).to_not be_valid
-  end
+    it 'icon should longer than 3 characters' do
+      subject.icon = 'ic'
+      expect(subject).to_not be_valid
+    end
 
-  it 'description should not be blank' do
-    subject.description = ''
-    expect(subject).to_not be_valid
-  end
+    it 'description should be present' do
+      subject.description = nil
+      expect(subject).to_not be_valid
+    end
 
-  it 'description should longer than 3 characters' do
-    subject.description = 'de'
-    expect(subject).to_not be_valid
-  end
+    it 'description should not be blank' do
+      subject.description = ''
+      expect(subject).to_not be_valid
+    end
 
-  it 'amount should not be less than 0' do
-    subject.amount = -5
-    expect(subject).to_not be_valid
-  end
+    it 'description should longer than 3 characters' do
+      subject.description = 'de'
+      expect(subject).to_not be_valid
+    end
 
-  it 'amount should not be nil' do
-    subject.amount = nil
-    expect(subject).to_not be_valid
+    it 'amount should not be less than 0' do
+      subject.amount = -5
+      expect(subject).to_not be_valid
+    end
+
+    it 'amount should not be nil' do
+      subject.amount = nil
+      expect(subject).to_not be_valid
+    end
   end
+  
 end
