32358822d9707335d48a355233e674e5f4348bff
c3c1l1a <cesswangumukima@gmail.com>
2022-11-04 16:32:50 +0800

    Refactor expenses view spec to use factory expense

--- spec/views/expenses/index.html.tailwindcss_spec.rb
+++ spec/views/expenses/index.html.tailwindcss_spec.rb
@@ -4,28 +4,20 @@ RSpec.describe 'expenses/index', type: :view do
   let(:user) do
     create(:user)
   end
+
+  
   before(:each) do
     assign(:expenses, [
-             Expense.create!(
-               name: 'Name',
-               description: 'Description',
-               icon: 'Icon',
-               user:
-             ),
-             Expense.create!(
-               name: 'Name',
-               description: 'Description',
-               icon: 'Icon',
-               user:
-             )
-           ])
+      create(:expense)
+    ])
   end
 
   it 'renders a list of expenses' do
     render
     cell_selector = Rails::VERSION::STRING >= '7' ? 'div>p' : 'tr>td'
-    assert_select cell_selector, text: Regexp.new('Name'.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new('Description'.to_s), count: 2
-    assert_select cell_selector, text: Regexp.new('Icon'.to_s), count: 2
+    assert_select cell_selector, text: Regexp.new('Name'.to_s), count: 1
+    assert_select cell_selector, text: Regexp.new('Description'.to_s), count: 1
+    assert_select cell_selector, text: Regexp.new('Icon'.to_s), count: 1
+    #assert_select cell_selector, text: Regexp.new(2.to_s), count: 2
   end
 end
